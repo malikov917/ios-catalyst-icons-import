1463e125fb1ca414e34afb6faee5739dc7894462
Kanstantsin Malikau <kanstanstin.malikau@haiilo.com>
2024-06-23 21:40:45 +0200

    Initial Commit

---
+++ AnotherIconImport/AnotherIconImportApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherIconImportApp.swift
+//  AnotherIconImport
+//
+//  Created by Kanstantsin Malikau on 23.06.24.
+//
+
+import SwiftUI
+
+@main
+struct AnotherIconImportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
